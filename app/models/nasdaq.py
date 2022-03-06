597fb66759f647893b46ff8dfbd3020d53307def
daniel <danirdelvalle@gmail.com>
2022-03-06 16:52:04 -0300

    Create new model

--- app/models/nasdaq.py
+++ app/models/nasdaq.py
@@ -1,6 +1,16 @@
 
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+
+class Orders(BaseModel):
+    symbol: str
+    price: float
+    shares: float
+    datetime: float
+    
+class DayValue(BaseModel):
+    day: str
+    value: float
 
 class TradeRequest(BaseModel):
     action: str
@@ -12,3 +22,26 @@ class TradeResponse(BaseModel):
     price: float
     shares: float
     usdAmount: float
+    
+class StocksRequest(BaseModel):
+    dateFrom: Optional[str]
+    dateTo: Optional[str]
+    symbol: str
+
+class StocksResponse(BaseModel):
+    dateFrom: Optional[str]
+    dateTo: Optional[str]
+    message: str
+    price: float
+    shares: float
+    usdAmount: float
+    buyOrders: List[Orders] = None
+    
+class HistoricRequest(BaseModel):
+    dateFrom: Optional[str]
+    dateTo: Optional[str]
+    symbol: str
+    
+class HistoricResponse(BaseModel):
+    symbol: str
+    values: List[DayValue] = None
